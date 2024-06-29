d32e808f2a2460dad0d2da3acc51393c0a613f63
Ayan Ozturk <ayan.ozturk@rewardgateway.com>
2024-06-29 20:13:44 +0100

    add cors

--- main.go
+++ main.go
@@ -4,12 +4,28 @@ import (
 	"collections/database"
 	"collections/handlers"
 	"collections/repositories"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"os"
+	"time"
 )
 
 func main() {
 	router := gin.Default()
+
+	// Configure CORS middleware
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"}, // Update with your frontend URL
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		//AllowOriginFunc: func(origin string) bool {
+		//	return origin == "*" // Update with your frontend URL
+		//},
+		MaxAge: 12 * time.Hour,
+	}))
+
 	dsn := os.Getenv("MYSQL_DSN")
 	if dsn == "" {
 		dsn = "app:app@tcp(localhost:3306)/app"
